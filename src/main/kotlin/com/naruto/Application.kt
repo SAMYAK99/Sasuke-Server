d86e32bd1db6c0c9dce5a67307282d16f84952b7
StreamerX <singhsamyak199@gmail.com>
2023-07-23 19:23:33 +0530

    Initial commit

---
+++ src/main/kotlin/com/naruto/Application.kt
@@ -0,0 +1,14 @@
+package com.naruto
+
+import io.ktor.server.application.*
+import com.naruto.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") 
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
