3d3f78c392a01e149c4d283b49b08d3c8d6ad118
StreamerX <singhsamyak199@gmail.com>
2023-09-03 16:11:16 +0530

    Added static images

--- src/main/kotlin/com/naruto/plugins/Routing.kt
+++ src/main/kotlin/com/naruto/plugins/Routing.kt
@@ -6,11 +6,13 @@ import com.naruto.route.searchHeroes
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
         searchHeroes()
+        staticResources(remotePath = "/images", basePackage = "images")
     }
 }
